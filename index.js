45e9a09b4eda8f3ab6a415432e30824a3e974278
Enrique FR <enriquefranciscoramirez95@gmail.com>
2021-09-28 10:32:26 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const cors = require('cors');
@@ -31,6 +32,11 @@ app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/upload', require('./routes/uploads'));
+
+// Lo último
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html'));
+});
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo ' +  process.env.PORT);
